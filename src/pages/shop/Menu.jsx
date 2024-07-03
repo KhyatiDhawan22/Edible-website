ca3eff49a84d0deb2546e1210d27e65f5d1ff3e4
Khyati Dhawan <112753105+KhyatiDhawan22@users.noreply.github.com>
2024-07-03 21:00:48 +0530

    Update Menu.jsx

--- src/pages/shop/Menu.jsx
+++ src/pages/shop/Menu.jsx
@@ -15,7 +15,7 @@ const Menu = () => {
     //fetch data from the backend
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:3000/menu");
+        const response = await fetch("/menu.json");
         const data = await response.json();
         // console.log(data)
         setMenu(data);
